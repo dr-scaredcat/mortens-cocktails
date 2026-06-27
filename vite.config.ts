d24471466b8511942ab76360640677e7f5cd2805
dr-scaredcat <mortenrose@protonmail.com>
2026-06-27 03:13:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
+  publicDir: "public",
   plugins: [
     tanstackStart({
       server: { entry: "server", preset: "cloudflare-workers" },
